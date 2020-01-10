f7cc79d57a22583e00ba495502be4d8ce709b99b
MrCrazyLeo <641512874@qq.com>
2020-01-10 16:22:25 +0800

    Peoject Travel init

--- Vue项目/travel/src/pages/home/Home.vue
+++ Vue项目/travel/src/pages/home/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <div class="home">home</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
 <style>
-
+  .home {
+    font-size: 50px
+  }
 </style>
