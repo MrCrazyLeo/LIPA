ebe021b54e17a02db4331bf2a25b209aef2972ba
MrCrazyLeo <641512874@qq.com>
2020-01-12 20:18:33 +0800

    add HomeRecommend

--- Vue项目/travel/src/pages/home/components/Icons.vue
+++ Vue项目/travel/src/pages/home/components/Icons.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="icons">
-  <swiper>
+  <swiper :options="swiperOption">
     <swiper-slide v-for="(page, index) of pages" :key="index">
     <div
       class='icon'
@@ -22,6 +22,10 @@ export default {
   name: 'HomeIcons',
   data () {
     return {
+      swiperOption: {
+        loop: false,
+        autoplay: false
+      },
       iconList: [{
         id: '0001',
         imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
@@ -83,33 +87,35 @@ export default {
 .icons >>> .swiper-container
   height 0
   padding-bottom 50%
-.icon
-  overflow hidden
-  position relative
-  float left
-  width 25%
-  height 0
-  padding-bottom 25%
-  .icon-img
-    position absolute
-    top 0
-    left 0
-    right 0
-    bottom .44rem
-    box-sizing border-box
-    padding .1rem
-    .icon-img-content
-      display block
-      margin 0 auto
-      height 100%
-  .icon-desc
-    position: absolute
-    left: 0
-    right: 0
-    bottom: 0
-    height: .44rem
-    line-height: .44rem
-    text-align: center
-    color: $darkTextColor
-    ellipsis()
+.icons
+  margin-top .1rem
+  .icon
+    overflow hidden
+    position relative
+    float left
+    width 25%
+    height 0
+    padding-bottom 25%
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom .44rem
+      box-sizing border-box
+      padding .1rem
+      .icon-img-content
+        display block
+        margin 0 auto
+        height 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      text-align: center
+      color: $darkTextColor
+      ellipsis()
 </style>
---
+++ Vue项目/travel/src/pages/home/components/Recommend.vue
@@ -0,0 +1,76 @@
+<template>
+<div>
+  <div class="title">热销推荐</div>
+  <ul>
+    <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+      <img class="item-img" :src="item.imgUrl" />
+      <div class="item-info">
+        <p class="item-title">{{item.title}}</p>
+        <p class="item-desc">{{item.desc}}</p>
+        <button class="item-button">查看详情</button>
+      </div>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+        title: '故宫',
+        desc: '东方宫殿建筑代表，世界宫殿建筑典范'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+        title: '故宫',
+        desc: '东方宫殿建筑代表，世界宫殿建筑典范'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+        title: '故宫',
+        desc: '东方宫殿建筑代表，世界宫殿建筑典范'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/mixins.styl'
+.title
+  line-height .8rem
+  background-color #eee
+  text-indent .2rem
+  margin-top .2rem
+.item
+  overflow hidden
+  display flex
+  height 1.84rem
+  .item-img
+    width 1.7rem
+    height 1.7rem
+    padding .07rem
+  .item-info
+    min-width 0
+    flex 1
+    padding .1rem
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color #ccc
+      ellipsis()
+    .item-button
+      margin-top .162rem
+      background-color #ff9300
+      padding 0 .1rem
+      border-radius .06rem
+      color #fff
+</style>
